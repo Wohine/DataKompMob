25014a08c743e8d5f0cbb355d7f46c3f9c36a34f
lund <christofferpaintball1@gmail.com>
2022-10-04 16:48:50 +0200

    lundern commit bestilling

--- app/src/main/java/com/example/datakompgaming/screen/ScreenMain.kt
+++ app/src/main/java/com/example/datakompgaming/screen/ScreenMain.kt
@@ -5,15 +5,19 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.datakompgaming.Routes
+import com.example.datakompgaming.bestillingfiler.Bestillinger
+import com.example.datakompgaming.bestillingfiler.printOrders
 
 @Composable
 fun ScreenMain(){
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = Routes.Login.route) {
-
         composable(Routes.Login.route) {
             LoginPage(navController = navController)
+        }
+        composable(Routes.BestillingComp.route){
+            printOrders(Bestillinger.bestilligListe)
         }
     }
 }
