40e5f5add8d4d19efda0d537ab5b5b6dd5cb6a79
ChristofferR <229537@student.usn.no>
2022-10-05 22:10:12 +0200

    Kundeservice :)

---
+++ app/src/main/java/com/example/datakompgaming/screen/Kundeservice.kt
@@ -0,0 +1,87 @@
+package com.example.datakompgaming.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.datakompgaming.R
+
+@Preview
+@Composable
+fun Kundeservice()
+{
+    Column( modifier = Modifier.background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    )
+    {
+
+        val navn = remember { mutableStateOf(TextFieldValue()) }
+        val email = remember { mutableStateOf(TextFieldValue()) }
+        val tema = remember { mutableStateOf(TextFieldValue()) }
+        val hjelptxt = remember { mutableStateOf(TextFieldValue()) }
+
+        Image(painter = painterResource(R.drawable.datakomplogo), contentDescription = null)
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Kontakt Oss", style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Bold))
+
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = "Hva lurer du på?", style = TextStyle(fontSize = 10.sp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(label = { Text(text = "Ditt navn") },
+            value = navn.value,
+            onValueChange = { navn.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(label = { Text(text = "Din email") },
+            value = email.value,
+            onValueChange = { email.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(label = { Text(text = "Tema") },
+            value = tema.value,
+            onValueChange = { tema.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(label = { Text(text = "Hva kan vi hjelpe deg med?") },
+            value = hjelptxt.value,
+            onValueChange = { hjelptxt.value = it })
+
+        Spacer(modifier = Modifier.height(25.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Send inn")
+            }
+        }
+
+
+
+    }
+}
