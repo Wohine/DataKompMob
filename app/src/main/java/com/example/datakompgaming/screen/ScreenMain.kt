002cb6e1754369658f61db09123b609ff58dcbd3
Wohine <wohine@gmail.com>
2022-10-25 16:53:43 +0200

    jøran er søt

--- app/src/main/java/com/example/datakompgaming/screen/ScreenMain.kt
+++ app/src/main/java/com/example/datakompgaming/screen/ScreenMain.kt
@@ -4,9 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.datakompgaming.HomePage
 import com.example.datakompgaming.Routes
-import com.example.datakompgaming.SampleData
 import com.example.datakompgaming.bestillingfiler.Bestillinger
 import com.example.datakompgaming.bestillingfiler.printOrders
 
