ac6ad6ac5383f4aa11c965f4ccfd1b9289a664d0
vehjerp <vehjerp@gmail.com>
2022-11-02 16:47:55 +0100

    homepag edit V

--- app/src/main/java/com/example/datakompgaming/screen/HomePage.kt
+++ app/src/main/java/com/example/datakompgaming/screen/HomePage.kt
@@ -3,6 +3,7 @@ package com.example.datakompgaming.screen
 
 
 import android.annotation.SuppressLint
+import android.view.Gravity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -10,8 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -21,6 +24,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.transition.Slide.GravityFlag
 import com.example.datakompgaming.R
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -66,18 +70,31 @@ fun LogoBanner(title: String) {
 @Composable
 fun Kort(imagePainter: Painter) {
     Column(modifier = Modifier
-        .padding(horizontal = 19.dp)
+        .padding(horizontal = 10.dp)
         .fillMaxHeight()
         .background(Color.Transparent),
     ) {
-        Text(text = "test test test")
+        Text(text = "testprodukt",
+            modifier = Modifier
+                .width(150.dp)
+                .absolutePadding(left = 38.dp),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Serif
+        )
         Image(painter = imagePainter, contentDescription = "test",
             modifier = Modifier
-                .fillMaxSize(),
+                .height(180.dp),
             alignment = Alignment.CenterStart
         )
+        Button(onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(150.dp)
+            .absolutePadding(left = 35.dp),
+        ) {
+            Text(text = "Kjøp")
+        }
     }
-
 }
 
 @Composable
@@ -109,9 +126,10 @@ fun PreHomePage() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             LogoBanner(title = "test")
-            FeatProd()
-            FeatProd()
-            FeatProd()
+            WelcomeSlider()
+            FeatProd("Gode Tilbud")
+            FeatProd("Ukens Produkter")
+            FeatProd("Våre Beste Produkter")
         }
     }
 }
@@ -119,14 +137,14 @@ fun PreHomePage() {
 @Composable
 fun ProduktSlider() {
     Row(modifier = Modifier
-        .height(150.dp)
+        .height(250.dp)
         .horizontalScroll(rememberScrollState(), enabled = true),
     ) {
-        Kort(painterResource(id = R.drawable.slider1))
-        Kort(painterResource(id = R.drawable.slider2))
-        Kort(painterResource(id = R.drawable.slider3))
-        Kort(painterResource(id = R.drawable.slider4))
-        Kort(painterResource(id = R.drawable.slider5))
+        Kort(painterResource(id = R.drawable.bilde1))
+        Kort(painterResource(id = R.drawable.bilde1))
+        Kort(painterResource(id = R.drawable.bilde1))
+        Kort(painterResource(id = R.drawable.bilde1))
+        Kort(painterResource(id = R.drawable.bilde1))
     }
 }
 
@@ -159,15 +177,15 @@ fun WelcomeSlider() {
 
     val coroutineScope = rememberCoroutineScope()
     //knapp boilerplate
-//    Button(
-//        onClick = {
-//            coroutineScope.launch {
-//                pagerState.animateScrollToPage(page = 2)
-//            }
-//        },
-//    ) {
-//        Text(text = "Scroll to the third page")
-//    }
+    Button(
+        onClick = {
+            coroutineScope.launch {
+                pagerState.animateScrollToPage(page = 2)
+            }
+       },
+    ) {
+        Text(text = "Scroll to the third page")
+ }
 }
 
 data class test123(
@@ -180,17 +198,17 @@ fun createItems() = listOf(test123(title = "Title1", subtitle = "Subtitle1", des
     test123(title = "Title2", subtitle = "Subtitle1", description = "Description1"),
     test123(title = "Title3", subtitle = "Subtitle1", description = "Description1")
 )
-@Preview
+//@Preview
 @Composable
-fun FeatProd() {
+fun FeatProd(title: String) {
     Column(
         modifier = Modifier
     ) {
-        Text(text = "Sample text",
+        Text(text = title,
             modifier = Modifier
                 .absolutePadding(bottom = Dp(5f))
                 .background(Color.Transparent),
-            textAlign = TextAlign.Right,
+            textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
             fontSize = 16.sp,
             color = Color(0xFFbd4fdb),
@@ -207,4 +225,4 @@ fun FeatProd() {
 
 // TODO: sliding cards
 // TODO: clickable images
-// TODO: drepe chris
+// TODO: drepe chris (done)
