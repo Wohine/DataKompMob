55a539b0fc12340b0132772650a3dc6823ef227a
Wohine <wohine@gmail.com>
2022-11-02 13:56:46 +0100

    data collection av flere dokument

--- app/src/main/java/com/example/datakompgaming/screen/Produkt.kt
+++ app/src/main/java/com/example/datakompgaming/screen/Produkt.kt
@@ -2,18 +2,21 @@ package com.example.datakompgaming.screen
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Source
+import java.util.stream.Collectors.toMap
 
 fun Produkter() {
 
     data class produkt(
-        val tittel: String = "",
-        val pris: Double = 0.0,
-        val varebeholdning: Int = 0,
-        val bilde: String = "",
-        val rating: Int = 0
+        val tittel: String? = "",
+        val pris: Double? = 0.0,
+        val varebeholdning: Int? = 0,
+        val bilde: String? = "",
+        val rating: Int? = 0
     ) {
         @Exclude
         fun toMap(): Map<String, Any?> {
@@ -27,22 +30,21 @@ fun Produkter() {
         }
     }
 
-    var firestore: FirebaseFirestore
-
-    firestore = FirebaseFirestore.getInstance()
+    var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     var docRef = firestore.collection("Produkter").document("NyeProdukter").collection("Hovedkort")
     var source = Source.DEFAULT
 
     docRef.get(source).addOnSuccessListener { documents ->
         for (document in documents) {
-            Log.d(TAG, "${document.id} => ${document.data}")
+            produkt(tittel = document.getString("tittel"))
+            produkt(pris = document.getDouble("pris"))
+            produkt(varebeholdning = document.get("varebeholdning") as Int?)
+            produkt(bilde = document.get("bilde") as String?)
+            produkt(rating = document.get("rating") as Int?)
         }
     }
     .addOnFailureListener { exception ->
         Log.w(TAG, "Error getting documents: ", exception)
     }
-
-
-
 }
